a5e104b24275091e87bb73a0f211b91970220c34
nutnicha.tul <nutnicha@azurecomputer.com>
2025-10-23 22:48:20 +0700

    first commit

---
+++ src/main/java/co/th/mcrop/mu_core/MuCoreApplication.java
@@ -0,0 +1,13 @@
+package co.th.mcrop.mu_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/th/mcrop/mu_core/MuCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package co.th.mcrop.mu_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
