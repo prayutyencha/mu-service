d4f1d41cc0dded6ba4ff616cdf75f32293c9c326
nutnicha.tul <nutnicha@azurecomputer.com>
2025-10-23 22:57:46 +0700

    inti mu controller

---
+++ src/main/java/co/th/mcrop/mu_core/controller/MuController.java
@@ -0,0 +1,17 @@
+package co.th.mcrop.mu_core.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/mu")
+public class MuController {
+
+    @GetMapping("/v1/number")
+    public String getMu(@PathVariable Integer number){
+        return number.toString();
+    }
+    
+}
