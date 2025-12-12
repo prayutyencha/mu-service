df3da6ce5bbd6b1d70b6672b9bd76cbf8608591c
prayutyencha <prayutyencha@gmail.com>
2025-12-12 17:09:31 +0700

    Update MuCoreApplicationTests.java
    fix error test

--- src/test/java/co/th/mcrop/mu_core/MuCoreApplicationTests.java
+++ src/test/java/co/th/mcrop/mu_core/MuCoreApplicationTests.java
@@ -3,7 +3,7 @@ package co.th.mcrop.mu_core;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = MuCoreApplication.class)
 class MuCoreApplicationTests {
 
 	@Test
